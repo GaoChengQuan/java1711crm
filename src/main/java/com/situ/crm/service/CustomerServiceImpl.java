489b93a66f4d53e9905c100cfd6763733c1c62a9
GaoChengquan <529390053@qq.com>
2018-03-21 17:29:45 +0800

    table 展示customer

---
+++ src/main/java/com/situ/crm/service/CustomerServiceImpl.java
@@ -0,0 +1,23 @@
+package com.situ.crm.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.situ.crm.common.DataGrideResult;
+import com.situ.crm.entity.Customer;
+import com.situ.crm.mapper.CustomerMapper;
+@Service
+public class CustomerServiceImpl implements ICustomerService{
+	@Autowired
+	private CustomerMapper customerMapper;
+
+	@Override
+	public DataGrideResult<Customer> pageList() {
+		int total = customerMapper.pageList().size();
+		List<Customer> rows = customerMapper.pageList();
+ 		return new DataGrideResult(total, rows);
+	}
+
+}
